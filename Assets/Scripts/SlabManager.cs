d9f6f0c6dee8b1fb19537aa6acba9261e9ea8f1d
Jaafoura <73950268+J43fura@users.noreply.github.com>
2026-01-22 03:03:51 +0100

    slab manager

--- Assets/Scripts/SlabManager.cs
+++ Assets/Scripts/SlabManager.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
-public class BombManager : MonoBehaviour
+public class SlabManager : MonoBehaviour
 {
     [Header("References")]
-    public GameObject bombPrefab;
+    public GameObject slabPrefab;
     public Transform player;
-    public GameObject explosionEffect;
 
     [Header("Spawn Settings")]
     public float minZDistance = 10f;
@@ -13,7 +12,6 @@ public class BombManager : MonoBehaviour
     public float laneDistance = 3f;
     public int lanes = 3;
     public float minSpawnGap = 25f;
-
 
     private float lastSpawnZ = 0f;
 
@@ -23,26 +21,19 @@ public class BombManager : MonoBehaviour
 
         if (player.position.z + maxZDistance > lastSpawnZ + minSpawnGap)
         {
-            SpawnBomb();
+            SpawnSlab();
         }
     }
 
-    void SpawnBomb()
+    void SpawnSlab()
     {
         int lane = Random.Range(0, lanes);
         float xPos = (lane - 1) * laneDistance;
         float zPos = lastSpawnZ + Random.Range(minZDistance, maxZDistance);
 
-        Vector3 spawnPos = new Vector3(xPos, 12f, zPos);
+        Vector3 spawnPos = new Vector3(xPos, 13f, zPos);
 
-        GameObject bomb = Instantiate(bombPrefab, spawnPos, Quaternion.identity);
-
-        // Assign explosion effect to bomb if it has Bomb script
-        Bomb bombScript = bomb.GetComponent<Bomb>();
-        if (bombScript != null && explosionEffect != null)
-        {
-            bombScript.explosionEffect = explosionEffect;
-        }
+        GameObject slab = Instantiate(slabPrefab, spawnPos, Quaternion.Euler(0f, 180f, 0f));
 
         lastSpawnZ = zPos;
     }
