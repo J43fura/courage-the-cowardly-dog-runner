edd58e5a4c40c36e940d9009fd044c796a224fd1
Jaafoura <73950268+J43fura@users.noreply.github.com>
2026-01-22 16:49:40 +0100

    fixes

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -29,7 +29,7 @@ public class TileManager : MonoBehaviour
 
     void Update()
     {
-        if (player.position.z + 40f > spawnZ)
+        if (player.position.z + 80f > spawnZ)
         {
             SpawnTile();
             DeleteOldTile();
